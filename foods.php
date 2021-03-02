3e8c02955323c8106e2dc57afa85d40eff519ba9
brieannabetts <bgbetts@lakeheadu.ca>
2021-03-02 14:49:01 -0600

    connection

--- foods.php
+++ foods.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Save Task</title>
+    <title>Foods</title>
 </head>
 <body>
 <?php
---
+++ select-cuisine.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Select Cuisine</title>
+</head>
+<body>
+<form method = 'post' action="'foods.php">
+    <fieldset>
+        <label for ="cuisineId"> Select a Cuisine</label>
+        <select name ="cuisineId" id="cuisineId">
+<?php
+//connect
+$db = new PDO('mysql:host=172.31.22.43;dbname=Brieanna1158732', 'Brieanna1158732', 'z0PV1TNW9O');
+
+?>
+</body>
+</html>
