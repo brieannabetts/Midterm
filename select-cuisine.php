516ca92ade5499eb306fafa17069537fe0fceb06
brieannabetts <bgbetts@lakeheadu.ca>
2021-03-02 14:56:33 -0600

    select-cuisine.php complete

--- select-cuisine.php
+++ select-cuisine.php
@@ -12,7 +12,20 @@
 <?php
 //connect
 $db = new PDO('mysql:host=172.31.22.43;dbname=Brieanna1158732', 'Brieanna1158732', 'z0PV1TNW9O');
-
+//query
+$sql = "SELECT cuisineId, name FROM cuisines";
+//command, execute query, store results
+$cmd = $db->prepare($sql);
+$cmd->execute();
+$cuisines =$cmd->fetchAll();
+//gather cuisines
+foreach ($cuisines as $cuisine){
+    echo'<option value="'.$cuisine['cuisineId'].'">'.$cuisine['name']. '</option>';
+}
+//disconnect
+$db = null;
 ?>
+        </select>
+    </fieldset>
 </body>
 </html>
