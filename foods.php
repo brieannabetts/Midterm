43912ccdedf4870dfe150bd8aad4a87b8441ea57
brieannabetts <bgbetts@lakeheadu.ca>
2021-03-02 15:27:36 -0600

    COMPLETE

--- foods.php
+++ foods.php
@@ -5,7 +5,7 @@
     <title>Foods</title>
 </head>
 <body>
-<h1>Foods</h1>
+<h1>Cuisine Choice</h1>
 <?php
 // get selected option
 $cuisineId = $_POST['cuisineId'];
@@ -14,12 +14,12 @@ $db = new PDO('mysql:host=172.31.22.43;dbname=Brieanna1158732', 'Brieanna1158732
 // query
 $sql = "SELECT * FROM cuisines WHERE cuisineId = :cuisineId";
 $cmd = $db->prepare($sql);
-$cmd->bindParam(':cuisineId', $cuisintId, PDO::PARAM_INT);
+$cmd->bindParam(':cuisineId', $cuisineId, PDO::PARAM_INT);
 //execute the query
 $cmd->execute();
 $cuisine = $cmd->fetch();
 //display the record
-echo '<h2> Cuisine: '. $cuisineId. '</h2>';
+echo '<h2> Cuisine: '. $cuisine['name']. '</h2>';
 //disconnect
 $db = null;
 ?>
